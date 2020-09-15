735f13471cd4fefaef1afee8142387f8836cb92e
AshokK <ashok.kumaraswamy@huawei.com>
2020-09-15 18:02:07 +0530

    gettoken method removed

--- Hms_wrapper_library/src/main/java/com/huawei/wrapper/hms_wrapper_library/HMS_Kits_Wrapper.java
+++ Hms_wrapper_library/src/main/java/com/huawei/wrapper/hms_wrapper_library/HMS_Kits_Wrapper.java
@@ -4,13 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.text.TextUtils;
-import android.util.Log;
 import android.widget.Toast;
-
-import com.huawei.agconnect.config.AGConnectServicesConfig;
-import com.huawei.hms.aaid.HmsInstanceId;
-import com.huawei.hms.common.ApiException;
 
 public class HMS_Kits_Wrapper {
     public static String TAG = HMS_Kits_Wrapper.class.getSimpleName();
@@ -31,29 +25,5 @@ public class HMS_Kits_Wrapper {
                 Toast.makeText(context, "Token: "+token, Toast.LENGTH_SHORT).show();
             }
         }
-    }
-
-    public void getToken(final Context context) {
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    // read from agconnect-services.json
-                    String appId = AGConnectServicesConfig.fromContext(context).getString("client/app_id");
-                    String token = HmsInstanceId.getInstance(context).getToken(appId, "HCM");
-                    Log.i(TAG, "get token:" + token);
-                    if(!TextUtils.isEmpty(token)) {
-                        sendRegTokenToServer(context, token);
-                    }
-                } catch (ApiException e) {
-                    Log.e(TAG, "get token failed, " + e);
-                }
-            }
-        }.start();
-    }
-
-    public void sendRegTokenToServer(Context context, String token) {
-        Log.i(TAG, "sending token to server. token:" + token);
-        Toast.makeText(context, "token: "+token, Toast.LENGTH_SHORT).show();
     }
 }
